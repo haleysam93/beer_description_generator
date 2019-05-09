9211f7bffa73a0c1cbafa6e0a96a623c85615e4c
Sam Haley <haleysam93@gmail.com>
2019-05-09 15:22:41 -0400

    Updated main.py to load the model from json instead of retraining it

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-"""My flask app."""
-
-import pandas as pd
+"""Web application that generates fake beer descriptions using markovify"""
+#import pandas as pd
 import markovify
 from flask import Flask
 app = Flask(__name__)
 
-beer_descs = pd.read_csv('beer_descs.csv', index_col=False)
-beer_desc_model = markovify.Text(beer_descs.text)
+#beer_descs = pd.read_csv('beer_descs.csv', index_col=False)
+with open('beer_desc_model.json', 'r') as f:
+    model_json = f.read()
+beer_desc_model = markovify.Text.from_json(model_json)
 
 @app.route('/')
 def generate_beer_desc():
